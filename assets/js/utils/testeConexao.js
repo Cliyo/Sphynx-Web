163a81891828193e5837f70cbb6c898a543bfb4b
Snootic <snooticonoob@hotmail.com>
2024-11-24 20:19:43 -0300

    refactor: update multicast port to a constant value

--- assets/js/utils/testeConexao.js
+++ assets/js/utils/testeConexao.js
@@ -4,6 +4,7 @@ const os = require('os');
 function multicast(req,res) {
     const message = Buffer.from('Request Sphynx API Address');
     const netInterfaces = os.networkInterfaces();
+    const port = 57128
     let responses = [];
     let pending = 0;
 
@@ -14,7 +15,7 @@ function multicast(req,res) {
                 pending++;
                 const client = dgram.createSocket('udp4');
                 client.bind({ address: address.address }, () => {
-                    client.send(message, 0, message.length, 57128, '239.255.255.250', (err) => {
+                    client.send(message, 0, message.length, port, '239.255.255.250', (err) => {
                         if (err) {
                             console.error('Error sending datagram:', err);
                             if (--pending === 0){
